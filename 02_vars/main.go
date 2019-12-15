bfcd4e67630e7b5625003b3ad733a88cfb2bb512
Vakishna Thayalan <vakishna@thayalantech.com>
2019-12-16 00:20:19 +1100

    Initialize

---
+++ 02_vars/main.go
@@ -0,0 +1,33 @@
+package main
+
+
+
+import "fmt"
+
+
+func main() {
+
+// Main Types:
+// string
+// bool
+// int
+// int int8 int16 int32 int64
+// uint unint8 uint16 uint32 uint64 uintptr
+// byte - alias for uint8
+// rune - alias for uint32
+// float32 float64
+// complex64 complex128
+
+var name string = "Vakishna"
+var age int32 = 22
+var isCool = true
+
+
+fmt.Println(name, age)
+fmt.Printf("%T\n", isCool)
+
+
+
+
+
+}
