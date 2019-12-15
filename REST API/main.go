0a2be9f3ca6117e2cf87d6365938c16eece6da68
Vakishna Thayalan <vakishna@thayalantech.com>
2019-12-16 00:40:43 +1100

    Route Handlers / Endpoints

--- REST API/main.go
+++ REST API/main.go
@@ -7,12 +7,35 @@ import (
   "net/http"
   "math/rand"
   "strconv"
+  "github.com/gorilla/mux"
 )
+
+// Book Struct (Model)
+
+
+
+
 
 
 func main() {
 
-  fmt.Println("Hello World")
+  // Initialize the Mux Router
+  // := is the type inference for GoLang
+  r := mux.NewRouter()
+
+  // Route Handlers / Endpoints
+  r.HandleFunc("/api/books", getBooks).Methods("GET")
+  r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+  r.HandleFunc("/api/books", createBook).Methods("POST")
+  r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+  r.HandleFunc("/api/book/{id}", deleteBook).Methods("DELETE")
+
+
+  // Run the server
+  http.ListenAndServe(":8080", r)
+
+
+
 
 
 
