758e4972f659217bed53413dc0a63c130f559c09
Vakishna Thayalan <vakishna@thayalantech.com>
2019-12-16 02:21:10 +1100

    CREATE Method Complete

--- REST API/main.go
+++ REST API/main.go
@@ -4,8 +4,8 @@ import (
   "encoding/json"
   "log"
   "net/http"
-  //"math/rand"
-  //"strconv"
+  "math/rand"
+  "strconv"
   "github.com/gorilla/mux"
 )
 
@@ -49,7 +49,11 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
  func createBook(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
-
+  var book Book
+  _ = json.NewDecoder(r.Body).Decode(&book)
+  book.ID = strconv.Itoa(rand.Intn(1000000)) // Mock ID - not safe
+  books = append(books, book)
+  json.NewEncoder(w).Encode(book)
 }
 
 // Update Book
